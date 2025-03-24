e0b10f642a3c7523524ed24152d9817f1f8cd496
CalvinMende <calvin.mende@capgemini.com>
2025-03-24 15:06:26 +0100

    feat: added new frontend

---
+++ frontend/src/types/todoProvider.d.ts
@@ -0,0 +1,23 @@
+interface TodoInterfaceI {
+  todos: TaskItemType[];
+
+  setTodos: React.Dispatch<React.SetStateAction<TaskItemType[]>>;
+  markComplete: (id: number) => void;
+  delTodo: (id: number) => void;
+  deleteAll: () => void;
+  editTodo: (id: number, text: string, deadline: string | null) => void;
+  addTodo: (title: string, deadline: string | null) => void;
+  moveTodo: (old: number, new_: number) => void;
+  markStar: (id: number) => void;
+  applyFilter: (
+    todos: TaskItemTypeI[],
+    filter: SelectedFilterE
+  ) => TaskItemTypeI[];
+  applySort: (
+    todos: TaskItemTypeI[],
+    sort: {
+      selectedSort: SelectedSortE;
+      selectedSortOrder: SelectedSortOrderE;
+    }
+  ) => TaskItemTypeI[];
+}
